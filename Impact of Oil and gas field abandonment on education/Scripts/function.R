3c9b96312dbfb5f360986ec9e009d9fc30c44d81
JAPJ182 <japj182@hotmail.com>
2023-01-16 15:29:53 -0300

    first commit

---
+++ Impact of Oil and gas field abandonment on education/Scripts/function.R
@@ -0,0 +1,13 @@
+
+#load("C:/Users/USER/Desktop/DID roads/did_roads/did_roads.RData")
+lista = c('readr','readxl','sqldf','plyr', 
+          'did' , 'arrow',  'plyr', 'ggplot2',
+          'dplyr','fixest' , 'gargle' , 'stringr'
+          #, 'bigrquery' 
+)
+for (i in 1:length(lista) ) {
+  if(lista[i] %in% rownames(installed.packages( lib.loc = "C:/Program Files/R/R-4.2.2/library")) == FALSE) {
+    install.packages(lista[i])
+  }
+  lapply(lista[i], library, character.only = TRUE)
+}
---
+++ README.txt
@@ -0,0 +1,3 @@
+#The Blessing of Oil Field
+In this study, In a series of two articles we used impact evaluation methods (Difference-in-differences in dynamic time and geographic regression discontinuity).
+In order to evaluate the effect of Oil or Gas fields in their exploration, production, and eventual abandonment stage on education outcomes in Colombia. 
